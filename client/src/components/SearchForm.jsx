0c0324fb54ca101678097d08fece0e509fe96fea
Fin M <qbres333@hotmail.com>
2024-12-10 21:14:35 -0500

    troubleshooting Atlas  DB connection user seeding

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -4,30 +4,29 @@
 
 // genre options
 const genreOptions = [
-  { key: 1, text: "", value: 1 },
-  { key: 2, text: "Action", value: 2 },
-  { key: 3, text: "Adventure", value: 3 },
-  { key: 4, text: "Animation", value: 4 },
-  { key: 5, text: "Biography", value: 5 },
-  { key: 6, text: "Comedy", value: 6 },
-  { key: 7, text: "Crime", value: 7 },
-  { key: 8, text: "Drama", value: 8 },
-  { key: 9, text: "Family", value: 9 },
-  { key: 10, text: "Fantasy", value: 10 },
-  { key: 11, text: "History", value: 11 },
-  { key: 12, text: "Horror", value: 12 },
-  { key: 13, text: "Music", value: 13 },
-  { key: 14, text: "Mystery", value: 14 },
-  { key: 15, text: "Romance", value: 15 },
-  { key: 16, text: "Sci - Fi", value: 16 },
-  { key: 17, text: "Short", value: 17 },
-  { key: 18, text: "Thriller", value: 18 },
-  { key: 19, text: "War", value: 19 },
+  { key: 1, text: "Action", value: 1 },
+  { key: 2, text: "Adventure", value: 2 },
+  { key: 3, text: "Animation", value: 3 },
+  { key: 4, text: "Biography", value: 4 },
+  { key: 5, text: "Comedy", value: 5 },
+  { key: 6, text: "Crime", value: 6 },
+  { key: 7, text: "Drama", value: 7 },
+  { key: 8, text: "Family", value: 8 },
+  { key: 9, text: "Fantasy", value: 9 },
+  { key: 10, text: "History", value: 10 },
+  { key: 11, text: "Horror", value: 11 },
+  { key: 12, text: "Music", value: 12 },
+  { key: 13, text: "Mystery", value: 13 },
+  { key: 14, text: "Romance", value: 14 },
+  { key: 15, text: "Sci - Fi", value: 15 },
+  { key: 16, text: "Short", value: 16 },
+  { key: 17, text: "Thriller", value: 17 },
+  { key: 18, text: "War", value: 18 },
+  { key: 19, text: "Western", value: 19 },
 ];
 
 // rating options
 const ratingOptions = [
-  { key: 1, text: "", value: 1 },
   { key: 1, text: "Abysmal", value: 1 },
   { key: 2, text: "Bad", value: 2 },
   { key: 3, text: "Average", value: 3 },
@@ -44,7 +43,7 @@ export default function SearchForm(props) {
 
             <div className="field genres">
               <div className="select-genre">
-                <select className="ui fluid dropdown">
+                <select className="dropdown">
                   <option value="" disabled selected>
                     Select Genre
                   </option>
