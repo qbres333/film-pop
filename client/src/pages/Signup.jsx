99037820e2157f1df753badb2d9befbec3b4e4e6
yinwu173 <yin.wu173@gmail.com>
2024-12-11 20:07:09 -0800

    added form inputs for username, email, and password to the sign up page.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { FormInput, Button, Form } from 'semantic-ui-react';
 
 
 export default function Signup() {
   return (
     <>
-      <h2>Signup Form</h2>
+      <Form >
+        <FormInput 
+          label={<label style={{ color: 'white' }}>Username</label>} 
+          placeholder='username'
+          fluid
+          id='form-input-username'
+        />
+        <FormInput 
+          label={<label style={{ color: 'white' }}>Email</label>} 
+          placeholder='joe@hotmail.com'
+          fluid
+        />
+        <FormInput 
+          label={<label style={{ color: 'white' }}>Password</label>} 
+          placeholder='*********' 
+          fluid
+        />
+        <Button>Submit</Button>
+      </Form>
     </>
   );
 }
