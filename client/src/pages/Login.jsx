c2bd7951987961c3d7f64591d171b7fed2ddd665
yinwu173 <yin.wu173@gmail.com>
2024-12-11 21:45:47 -0800

    Added a auth.js file to manage user authentification, added a mutation for user login, created login form, polished signup page UI, and login form submission logic.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,24 @@
+import React from 'react'
+import { FormInput, Button, Form } from 'semantic-ui-react';
+
+
 export default function Login() {
-    return (
-      <>
-        Login
-      </>
-    );
+  return (
+    <>
+      <Form >
+        <FormInput
+          label={<label style={{ color: 'white' }}>Email</label>}
+          placeholder='joe@hotmail.com'
+          fluid
+        />
+        <FormInput
+          label={<label style={{ color: 'white' }}>Password</label>}
+          placeholder='*********'
+          fluid
+        />
+
+        <Button inverted color='blue' size='large' floated='left'>Login</Button>
+      </Form>
+    </>
+  );
 }
